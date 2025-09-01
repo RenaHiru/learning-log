b953e1e5f4db8d032252a9f95cb9a6d9dddf5acb
RenaHiru <papa.chibi.biscuit20201223@gmail.com>
2025-09-01 20:44:18 +0900

    Day2: Pythonの変数と型(int, str, list, dict)

---
+++ day02_variables.py
@@ -0,0 +1,15 @@
+# 整数
+age = 32
+print("年齢:",age)
+
+# 文字列
+name = "Rena"
+print("名前:",name)
+
+# リスト
+scores = [80,90,100]
+print("スコアリスト:",scores)
+
+# 辞書(キーと値の組み合わせ)
+student = {"name":"name","age":age,"score":scores}
+print("学生情報:",student)
